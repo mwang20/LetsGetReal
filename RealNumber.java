3f38118adb30fda99e251968311f1db3c6549721
mwang20 <mwang20@stuy.edu>
2020-11-30 10:02:35 -0500

    I'm pretty sure I am done with the assignment, since my toString method in my RationalNumber file already accounts for denominator of 1 and numerator of 0 since it returns the original numerator and denominator and a / string in between them.

--- RealNumber.java
+++ RealNumber.java
@@ -1,6 +1,6 @@
 import java.lang.Math;
 
-public class RealNumber{
+public class RealNumber extends Number{
   private double value;
   public RealNumber(double v){
     value = v;
@@ -12,21 +12,6 @@ public class RealNumber{
 
   public String toString(){
     return ""+getValue();
-  }
-
-  public boolean equals(RealNumber other){
-    double a = this.getValue();
-    double b = other.getValue();
-    if (a == 0 || b == 0){
-      if (Math.abs(a - b) == 0){
-        return true;
-      }
-      else return false;
-    }
-    if (Math.abs(a - b) <= 0.001){
-      return true;
-    }
-    return false;
   }
 
   public RealNumber add(RealNumber other){
