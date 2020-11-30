caf821986b52b56bfdf0697dc7526373103eaa6b
mwang20 <mwang20@stuy.edu>
2020-11-30 00:40:44 -0500

    Moved the equals method from the RealNumber class to the Number abstract class.

--- Number.java
+++ Number.java
@@ -1,3 +1,19 @@
 public abstract class Number{
+
+  public abstract double getValue();
   
+  public boolean equals(RealNumber other){
+    double a = this.getValue();
+    double b = other.getValue();
+    if (a == 0 || b == 0){
+      if (Math.abs(a - b) == 0){
+        return true;
+      }
+      else return false;
+    }
+    if (Math.abs(a - b) <= 0.001){
+      return true;
+    }
+    return false;
+  }
 }
